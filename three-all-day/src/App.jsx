2bfae7f953edb476c494a29c67765dfa7142e049
PhoebeTracyChaoKazungu <kazungutracy711@gmail.com>
2023-04-27 12:52:32 +0300

    Pro

--- three-all-day/src/App.jsx
+++ three-all-day/src/App.jsx
@@ -1,35 +1,52 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import hobbies from "./db.json"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      const handleAddHobbies = (hobbies) => {
+		setHobbies([...hobbies, hobbies]);
+	};
+	const handleDeleteHobbies = (hobbiesId) => {
+		const filterHobbies = hobbies.filter(
+			(hobbies) => hobbies.id !== hobbiesId
+		);
+
+		setHobbies(filterHobbies);
+	};
+	const handleSearch = (searchTerm) => {
+		if (searchTerm) {
+			const filteredHobbies = Hobbies.filter((hobbies) => {
+				if (hobbies.description.toLowerCase().match(searchTerm.toLowerCase())) {
+					return true;
+				} else {
+					return false;
+				}
+			});
+			setHobbies(filteredHobbies);
+		} else {
+			fetchHobbies();
+		}
+	};
+	return (
+		<div className="ui raised segment">
+			<div className="ui segment violet inverted">
+				<h2>Three All Day</h2>
+			</div>
+			<AccountContainer
+				handleAddTransaction={handleAddHobbies}
+				hobbies={hobbies}
+				handleSearch={handleSearch}
+				handleDeleteHobbies={handleDeleteHobbies}
+			/>
+       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
-  )
+  );
 }
 
-export default App
+
+export default App;
